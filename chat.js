00a5688db3492d821ca5f845ff55ab1c7cc8d5f2
Ana Bajo <kozicana@yahoo.com>
2021-03-19 15:02:37 +0100

    frontend challenge, both pages

---
+++ chat.js
@@ -0,0 +1,31 @@
+const messages = document.getElementById("messages");
+const textbox = document.getElementById("textbox");
+
+
+textbox.addEventListener("keypress", function(event){
+  if (event.keyCode === 13) {
+    const newContainer = document.createElement("div");
+    newContainer.classList.add("message");
+    const newMessage = `<div class="message" >
+                          <a href="index.html">
+                            <img src="img/user.jpeg" alt="portrait" class="img-circle">
+                          </a>
+                          <div class="bubble left "><p> ${textbox.value} </p></div>
+                        </div>`
+    messages.insertAdjacentHTML("beforeend", newMessage);
+    messages.lastElementChild.scrollIntoView();
+    textbox.value = "";
+  }
+});
+
+setTimeout(() => {
+  const fakeMessage = `<div class="message right" >
+                          <i class="fas fa-circle small"></i>
+                          <a href="index.html">
+                            <img src="img/profpic.jpg" alt="portrait" class="img-circle">
+                          </a>
+                          <div class="bubble right "><p>Mauris et arcu erat. Nulla ac accumsan risus. </p></div>
+                      </div>`
+  messages.insertAdjacentHTML("beforeend", fakeMessage);
+}, 1500)
+
