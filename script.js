2a6bdd0c00c5a4d519eff4f73f2bd049fda30775
Ana Bajo <kozicana@yahoo.com>
2021-03-21 16:12:43 +0100

    added README.md, modified script.js and corresponding CSS

--- script.js
+++ script.js
@@ -2,18 +2,8 @@
 const btnAdd = document.querySelector("#add");
 const wrapper = document.querySelector(".wrapper");
 
-let colorState = 0;
 
 btnAdd.addEventListener("click", () => {
-  if (colorState == 0) {
-    btnAdd.style.backgroundColor = "#d38da3";
-    wrapper.style.backgroundColor = "#d8f3dc";
-    colorState = 1;
-  } else {
-    btnAdd.style.backgroundColor = "#3c3c3c";
-    wrapper.style.backgroundColor = "#e8e8e8";
-    colorState = 0;
-  }
+  btnAdd.classList.toggle("btnChangeColor");
+  wrapper.classList.toggle("wrapperChangeColor");
 });
-
-
